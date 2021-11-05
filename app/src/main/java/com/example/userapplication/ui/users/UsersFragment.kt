0ff1c864e6c170a6067e5956365af78ed3d3f7df
User <pertiamariami@gmail.com>
2021-11-05 09:44:55 +0400

    design

--- app/src/main/java/com/example/userapplication/ui/users/UsersFragment.kt
+++ app/src/main/java/com/example/userapplication/ui/users/UsersFragment.kt
@@ -10,6 +10,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.userapplication.R
 import com.example.userapplication.databinding.FragmentUsersBinding
 import com.example.userapplication.date.Resource
 import com.example.userapplication.model.UserRespons
@@ -40,12 +41,15 @@ class UsersFragment : Fragment() {
         viewModel.usersRespons.observe(viewLifecycleOwner) {
             when (it.status) {
                 Resource.Status.LOADING -> {
+                    binding.progress.progress.visibility =View.VISIBLE
 
                 }
                 Resource.Status.ERROR -> {
-                    //todo loader and error message
+                    binding.progress.progress.visibility = View.GONE
+                    Toast.makeText(context, getString(R.string.error_text), Toast.LENGTH_SHORT).show()
                 }
                 Resource.Status.SUCCESS -> {
+                    binding.progress.progress.visibility =View.GONE
                     users = it.data!!
                     adapter = UsersAdapter(requireContext(), it.data!!)
                     setupRecycler()
